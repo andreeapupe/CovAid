cccb7a0e136f629fc082c1b7ff5233909a2bb529
andreeapupe <andreea.pupe99@e-uvt.ro>
2021-03-01 20:21:44 +0200

    Switched login element & updated info page

--- src/app/PATIENT_RELATED/info-page/info-page.component.ts
+++ src/app/PATIENT_RELATED/info-page/info-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-info-page',
   templateUrl: './info-page.component.html',
-  styleUrls: ['./info-page.component.css']
+  styleUrls: ['./info-page.component.css'],
+  animations: [
+    trigger('flipState', [
+      state(
+        'active',
+        style({
+          transform: 'rotateY(180deg)',
+        })
+      ),
+      state(
+        'inactive',
+        style({
+          transform: 'rotateY(0)',
+        })
+      ),
+      transition('active => inactive', animate('700ms ease-out')),
+      transition('inactive => active', animate('600ms ease-in')),
+    ]),
+  ],
 })
 export class InfoPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  flip: string = 'inactive'
+
+  toggleFlip() {
+    this.flip = this.flip == 'inactive' ? 'active' : 'inactive'
   }
-
 }

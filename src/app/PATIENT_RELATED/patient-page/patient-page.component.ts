48b4dfce5659caab90e34cfadc6df7295acbec09
andreeapupe <andreea.pupe99@e-uvt.ro>
2021-03-19 14:06:28 +0200

    Added sticky toolbars

--- src/app/PATIENT_RELATED/patient-page/patient-page.component.ts
+++ src/app/PATIENT_RELATED/patient-page/patient-page.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
 @Component({
   selector: 'app-patient-page',
   templateUrl: './patient-page.component.html',
-  styleUrls: ['./patient-page.component.css']
+  styleUrls: ['./patient-page.component.css'],
 })
 export class PatientPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

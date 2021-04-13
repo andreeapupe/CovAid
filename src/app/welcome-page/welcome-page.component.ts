d63989bfe82cf3b6e3206b44da4669e071b7e51a
Andreea <andreeapupe@yahoo.com>
2021-04-13 20:24:41 +0300

    Adapted content
    + Implemented Sidenav on all pages
    + Removed scroll-to-top feature (stopped working because of sidenav)
    + Reorganised file structure

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Inject, HostListener } from '@angular/core'
-import { DOCUMENT } from '@angular/common'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-welcome-page',
@@ -7,35 +6,7 @@ import { DOCUMENT } from '@angular/common'
   styleUrls: ['./welcome-page.component.css'],
 })
 export class WelcomePageComponent implements OnInit {
-  windowScrolled: boolean
-  constructor(@Inject(DOCUMENT) private document: Document) {}
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    if (
-      window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop > 100
-    ) {
-      this.windowScrolled = true
-    } else if (
-      (this.windowScrolled && window.pageYOffset) ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop < 10
-    ) {
-      this.windowScrolled = false
-    }
-  }
-  scrollToTop() {
-    ;(function smoothscroll() {
-      var currentScroll =
-        document.documentElement.scrollTop || document.body.scrollTop
-      if (currentScroll > 0) {
-        window.requestAnimationFrame(smoothscroll)
-        window.scrollTo(0, currentScroll - currentScroll / 10)
-      }
-    })()
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
